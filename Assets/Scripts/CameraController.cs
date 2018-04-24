a4a818e252c5c8b9b7d53fd92637e3125b6d48b2
Kodereczek <polecpatryk@gmail.com>
2018-04-24 22:36:04 +0200

    Camera update

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,18 +4,39 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Transform target;
 
-    private Transform transformCamera;
-    private GameObject player;
+    public Vector3 offset = new Vector3(1.8f, -1.2f, 0.0f);
 
-    void Start ()
+    public float zoomSpeed = 15f;
+    public float zoomMin = 2f;
+    public float zoomMax = 40f;
+
+    public float pitch = 2f;
+
+    public float yawSpeed = 150f;
+
+    private float currentZoom = 15f;
+    private float currentYawHorizontal = 0f;
+
+    void Start()
     {
-        transformCamera = GetComponent<Transform>();
-        player = GameObject.FindGameObjectWithTag("Player");
+
     }
 	
-	void Update ()
+    void Update()
     {
-        transformCamera.position = player.transform.position + new Vector3(20.0f, 40.0f, 20.0f);
-	}
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, zoomMin, zoomMax);
+
+        currentYawHorizontal -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+    }
+
+	void LateUpdate ()
+    {
+        transform.position = target.position - offset * currentZoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
+
+        transform.RotateAround(target.position, Vector3.up, currentYawHorizontal);
+    }
 }
